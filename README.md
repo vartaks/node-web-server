9aedbd0fca16978b634e3888abb2f214c033e828
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-08-14 16:22:07 +0530

    Added Handlebars partials and Handlebars helpers

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-web-server
 Web server app using NodeJS
+
+Run using following command
+$ nodemon server.js -e js,hbs
+
--- server.js
+++ server.js
@@ -3,21 +3,28 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Welcome to Sourabh web portal'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
